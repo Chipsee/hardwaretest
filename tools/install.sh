5f0f25567f8394bf08910d6fa3593b7fb1ae8097
Xiaoqiang Liu <lxq@chipsee.com>
2022-11-30 18:06:36 +0800

    Modify install script for multi arch system

--- tools/install.sh
+++ tools/install.sh
@@ -5,6 +5,12 @@
 # qmake && make
 
 KVR=`uname -r`
+LARCH=`uname -m`
+if [ $LARCH = "aarch64" ]; then
+LIBDIR="aarch64-linux-gnu"
+else
+LIBDIR="arm-linux-gnueabihf"
+fi
 
 if [ ! -f hardwaretest -a ! -f LCDTester/LCDTester ]; then
 	echo "no binary, compile them first"
@@ -21,29 +27,29 @@ fi
 mkdir -p usr/share/applications
 mkdir -p usr/share/pixmaps
 mkdir -p usr/bin/
-mkdir -p usr/lib/arm-linux-gnueabihf/qt5/qml/QtQml
-mkdir -p usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick
-mkdir -p usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2
-mkdir -p usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2
+mkdir -p usr/lib/${LIBDIR}/qt5/qml/QtQml
+mkdir -p usr/lib/${LIBDIR}/qt5/qml/QtQuick
+mkdir -p usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2
+mkdir -p usr/lib/${LIBDIR}/qt5/qml/QtQuick.2
 sudo cp resourcefiles/hardwaretest usr/ -rf
 sudo cp hardwaretest usr/hardwaretest/ -rf
 sudo cp usr/hardwaretest/hardwaretest.desktop usr/share/applications/
 sudo cp usr/hardwaretest/hardwaretest.png usr/share/pixmaps/
 sudo cp LCDTester/LCDTester usr/bin/
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/builtins.qmltypes usr/lib/arm-linux-gnueabihf/qt5/qml/builtins.qmltypes
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQml/plugins.qmltypes usr/lib/arm-linux-gnueabihf/qt5/qml/QtQml/plugins.qmltypes
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQml/qmldir usr/lib/arm-linux-gnueabihf/qt5/qml/QtQml/qmldir
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/libwindowplugin.so usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/libwindowplugin.so
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/plugins.qmltypes usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/plugins.qmltypes
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/qmldir usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick/Window.2/qmldir
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/libqtquick2plugin.so usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/libqtquick2plugin.so
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/plugins.qmltypes usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/plugins.qmltypes
-sudo cp /usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/qmldir usr/lib/arm-linux-gnueabihf/qt5/qml/QtQuick.2/qmldir
-sudo cp -a /usr/lib/arm-linux-gnueabihf/libQt5Qml.so* usr/lib/arm-linux-gnueabihf/
-sudo cp -a /usr/lib/arm-linux-gnueabihf/libQt5Quick.so* usr/lib/arm-linux-gnueabihf/
-sudo cp -a /usr/lib/arm-linux-gnueabihf/libQt5QmlModels.so* usr/lib/arm-linux-gnueabihf/
-sudo cp -a /usr/lib/arm-linux-gnueabihf/libQt5QmlWorkerScript.so* usr/lib/arm-linux-gnueabihf/
-sudo cp -a /usr/lib/arm-linux-gnueabihf/libQt5SerialPort.so* usr/lib/arm-linux-gnueabihf/
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/builtins.qmltypes usr/lib/${LIBDIR}/qt5/qml/builtins.qmltypes
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQml/plugins.qmltypes usr/lib/${LIBDIR}/qt5/qml/QtQml/plugins.qmltypes
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQml/qmldir usr/lib/${LIBDIR}/qt5/qml/QtQml/qmldir
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/libwindowplugin.so usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/libwindowplugin.so
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/plugins.qmltypes usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/plugins.qmltypes
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/qmldir usr/lib/${LIBDIR}/qt5/qml/QtQuick/Window.2/qmldir
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/libqtquick2plugin.so usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/libqtquick2plugin.so
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/plugins.qmltypes usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/plugins.qmltypes
+sudo cp /usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/qmldir usr/lib/${LIBDIR}/qt5/qml/QtQuick.2/qmldir
+sudo cp -a /usr/lib/${LIBDIR}/libQt5Qml.so* usr/lib/${LIBDIR}/
+sudo cp -a /usr/lib/${LIBDIR}/libQt5Quick.so* usr/lib/${LIBDIR}/
+sudo cp -a /usr/lib/${LIBDIR}/libQt5QmlModels.so* usr/lib/${LIBDIR}/
+sudo cp -a /usr/lib/${LIBDIR}/libQt5QmlWorkerScript.so* usr/lib/${LIBDIR}/
+sudo cp -a /usr/lib/${LIBDIR}/libQt5SerialPort.so* usr/lib/${LIBDIR}/
 sync
 tar zcvf hardwaretest-app-${KVR}.tar.gz usr
 sync
